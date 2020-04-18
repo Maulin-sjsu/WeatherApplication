472f43f8191a5605f6a95d4b5897fa6e255edcc3
Maulin Bodiwala <maulin.bodiwala@sjsu.edu>
2020-04-18 12:38:18 -0700

    Adding Static html pages

--- WebServer/src/app.js
+++ WebServer/src/app.js
@@ -1,22 +1,42 @@
+const path = require('path')
 const express = require('express')
+
+//console.log(__dirname)
+const pubicDirectoryPath = path.join(__dirname,'../public')
 
 const app = express();
 
+
+app.use(express.static(pubicDirectoryPath))
+
 const port = 3000;
-app.get('',(req, res)=>{
-    res.send('Hello Express !')
-})
 
-app.get('/help',(req, res)=>{
-    res.send('Help Page !')
-})
+// app.get('',(req, res)=>{
+//     res.send('<h1> Hello Express </h1>')
+// })
 
-app.get('/about',(req,res)=>{
-    res.send('About')
-})
+// app.get('/help',(req, res)=>{
+//     res.send([{
+//         name: 'Maulin',
+//         age: 25
+//     },{
+//         name: 'Darshil',
+//         age: 25
+//     },{
+//         name: 'Dhruvil',
+//         age: 23
+//     }])
+// })
+
+// app.get('/about',(req,res)=>{
+//     res.send('About')
+// })
 
 app.get('/weather',(req,res)=>{
-    res.send("Today's Weather Information")
+    res.send({
+        forecast: 'clody outside with 12 degrees out there',
+        location: 'San Jose'
+    })
 })
 
 
