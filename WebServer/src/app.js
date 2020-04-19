12df49e65772f31367d523513c7a2e8baa2181aa
Maulin Bodiwala <maulin.bodiwala@sjsu.edu>
2020-04-19 02:09:33 -0700

    Weather APIS wired up

--- WebServer/src/app.js
+++ WebServer/src/app.js
@@ -1,6 +1,10 @@
 const path = require('path')
 const express = require('express')
 const hbs =  require('hbs')
+const request = require('request')
+const geocode = require('./utils/geoCode')
+const forcast = require('./utils/forcast')
+
 
 //console.log(__dirname)
 
@@ -42,9 +46,46 @@ app.get('/about',(req,res)=>{
 })
 
 app.get('/weather',(req,res)=>{
+    if(!req.query.address){
+        return res.send({
+            error: "You must provide a search term"
+        })
+    }
+    geocode(req.query.address, (error, {longitude, latitude, location})=>{
+        if (error)
+        {
+            return res.send({error})
+        }
+        //console.log('data', data)
+        forcast(longitude, latitude, (error, forcastdata) => {
+           if(error)
+           {
+            return res.send({error})
+           }res.send({
+                Location :  location,
+                forcast :  forcastdata,
+                address: req.query.address
+           })
+ 
+        })
+    })
+    
+    // res.send({
+    //     forecast: 'clody outside with 12 degrees out there',
+    //     location: 'San Jose',
+    //     address: req.query.address
+    // })
+})
+
+app.get('/products',(req,res)=>{
+    if(!req.query.search){
+        return res.send({
+            error: "You must provide a search term"
+        })
+    }
+    console.log(req.query.search)
     res.send({
-        forecast: 'clody outside with 12 degrees out there',
-        location: 'San Jose'
+        products:[]
     })
 })
 
